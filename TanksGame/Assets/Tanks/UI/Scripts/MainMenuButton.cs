99d75402da429200c165e92b5bb291fcd93b399b
Antonio Omazić <73439988+aomazic@users.noreply.github.com>
2025-02-25 22:25:40 +0100

    Buttons vfx and sfx

---
+++ TanksGame/Assets/Tanks/UI/Scripts/MainMenuButton.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class MainMenuButton : MonoBehaviour, IPointerEnterHandler
+{
+    [Header("Audio")]
+    [SerializeField] private AudioClip hoverSound;
+    [SerializeField] private AudioClip clickSound;
+
+    [Header("References")]
+    [SerializeField] private AudioSource audioSource;
+    
+    private Button button;
+    
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        
+        button.onClick.AddListener(PlayClickSound);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (hoverSound != null)
+        {
+            audioSource.PlayOneShot(hoverSound);
+        }
+    }
+
+    private void PlayClickSound()
+    {
+        if (clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        button.onClick.RemoveListener(PlayClickSound);
+    }
+}
